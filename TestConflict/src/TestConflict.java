cb7523b89e7475fe78298e16097e40c089dec613
OlehDemishev <oleh.demishev@gmail.com>
2024-10-29 10:02:01 +0100

    g

---
+++ TestConflict/src/TestConflict.java
@@ -0,0 +1,14 @@
+public class TestConflict {
+
+    public void testA(){
+
+        System.out.println("1");
+        //TODO  Imp;ement testA
+    }
+
+    public void testB() {
+
+        //TODO  Imp;ement testB
+
+    }
+}
